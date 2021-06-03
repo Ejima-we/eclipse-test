5d890e4c2a1a3f11c21c46a94f27e86ae172a0ad
Ejima-we <76987734+Ejima-we@users.noreply.github.com>
2021-06-03 17:16:38 +0900

    finish 04

---
+++ src/Abb/Chepter04.java
@@ -0,0 +1,13 @@
+package Abb;
+
+public class Chepter04 {
+	public static void main(String[] args) {  
+		String name = "AG";
+		String lang = "日本";
+		int num = 3;
+		
+		System.out.println(name);
+		System.out.println(lang);
+		System.out.println(num);
+	}
+}	
