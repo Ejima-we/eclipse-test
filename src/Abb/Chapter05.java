b15233e257d595fe017c576d1c54db042e10b096
Ejima-we <76987734+Ejima-we@users.noreply.github.com>
2021-06-03 20:10:48 +0900

    finish 05

---
+++ src/Abb/Chapter05.java
@@ -0,0 +1,23 @@
+package Abb;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 10000;
+		double d = 10.5;
+		boolean b = true;
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		
+		Float f = 10.1f;
+		Integer i = 100;
+		Character c = 'う';
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+		
+		int[] array = {10, 20, 30, 40, 50};
+		System.out.println(array[3]);
+		
+	}
+}
