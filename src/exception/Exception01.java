bcf8f8a0e8d1785519e2de46046a070945fb34aa
Ejima-we <76987734+Ejima-we@users.noreply.github.com>
2021-06-06 14:50:02 +0900

    finish 10-1

---
+++ src/exception/Exception01.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class Exception01 {
+	public static void main(String[] args) {
+		System.out.println("100 ÷ 0 は？");
+		int reslt = 100 / 0;
+		System.out.println("計算結果" + reslt);
+		System.out.println("プログラムを終了します");
+	}
+}
