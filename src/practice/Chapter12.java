4a05315c7c62298961a93dfc62d21aba150fbcba
Ejima-we <76987734+Ejima-we@users.noreply.github.com>
2021-06-07 16:44:31 +0900

    finish 12

--- src/practice/Chapter12.java
+++ src/practice/Chapter12.java
@@ -1,5 +1,12 @@
 package practice;
 
 public class Chapter12 {
-
+	public static void main(String[] args ) {
+		CapsuleMan human1 = new CapsuleMan("小林", 32);
+		System.out.println("名前は" + human1.getName() + "です");
+		System.out.println("年齢は" + human1.getAge() + "です");
+		
+		human1.setAge(33);
+		System.out.println("誕生日がきたので" + human1.getAge() + "になりました");
+	}
 }
