79255c19b46345e756941beaedb2f6c26bf21e3b
Ejima-we <76987734+Ejima-we@users.noreply.github.com>
2021-06-17 09:40:16 +0900

    Finish

--- src/practice/FizzBuzz5.java
+++ src/practice/FizzBuzz5.java
@@ -1,5 +1,15 @@
 package practice;
 
 public class FizzBuzz5 {
-
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 42 == 0) {
+				System.out.println(i + " = FizzBuzz");
+			} else if (i % 7 == 0) { 
+				System.out.println(i + " = Buzz");
+			} else if (i % 6 == 0) {
+				System.out.println(i + " = Fizz");
+			}
+		}
+	}
 }
