20954984c80e2d276db262bf49ad7fb834ddbca7
Ejima-we <76987734+Ejima-we@users.noreply.github.com>
2021-06-21 18:28:31 +0900

    Finish

--- src/practice/triangle4.java
+++ src/practice/triangle4.java
@@ -1,5 +1,18 @@
 package practice;
 
 public class triangle4 {
-
+	public static void main(String[] args) {
+		int max = 6;
+		String star = "*";
+		
+		for (int i = 1; i <= max; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= max*2-(i*2)-2;k++) {
+				System.out.print(star);
+			}
+			System.out.println();
+		}
+	}
 }
